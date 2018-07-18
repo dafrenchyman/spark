c59a018f9cf55fdf57ad40e4971852b44cd76e70
Julien Pierret <dafrenchyman@gmail.com>
2018-07-17 17:53:28 -0700

    Found more issues with Floor/Ceiling/Missing. The current codeb ase also didn't scale correctly on the percentiles when running on a "real" large dataset. Fixed that so it no longer "collects" stuff.

--- src/main/java/com/mrsharky/spark/ml/feature/FloorCeilingMissing.java
+++ src/main/java/com/mrsharky/spark/ml/feature/FloorCeilingMissing.java
@@ -24,15 +24,15 @@
 package com.mrsharky.spark.ml.feature;
 
 import com.mrsharky.spark.ml.feature.models.FloorCeilingMissingModel;
-import com.mrsharky.spark.ml.param.DoubleParam2;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
 import java.util.UUID;
-import org.apache.commons.math3.stat.descriptive.rank.Percentile;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.ml.Estimator;
 import org.apache.spark.ml.param.DoubleParam;
-import org.apache.spark.ml.param.Param;
 import org.apache.spark.ml.param.ParamMap;
 import org.apache.spark.ml.param.StringArrayParam;
 import org.apache.spark.ml.util.MLReader;
@@ -41,6 +41,9 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.types.StructType;
 import org.apache.spark.ml.util.DefaultParamsWritable;
+import static org.apache.spark.sql.functions.col;
+import org.apache.spark.storage.StorageLevel;
+import scala.Tuple2;
 
 /**
  *
@@ -149,108 +152,128 @@ public class FloorCeilingMissing extends Estimator<FloorCeilingMissingModel> imp
     @Override
     public StructType transformSchema(StructType oldSchema) {
         return oldSchema;
-    }    
+    }
+    
+    /**
+     * https://stackoverflow.com/questions/28805602/how-to-compute-percentiles-in-apache-spark
+     * @param rdd
+     * @param percentiles
+     * @param rddSize
+     * @param numPartitions
+     * @return 
+     */
+    private static Double[] getPercentiles(JavaRDD<Double> rdd, Double[] percentiles) {
+        long rddSize = rdd.count();
+        int numPartitions = rdd.getNumPartitions();
+        Double[] values = new Double[percentiles.length];
+        JavaRDD<Double> sorted = rdd.sortBy((Double d) -> d, true, numPartitions);
+        JavaPairRDD<Long, Double> indexed = sorted.zipWithIndex().mapToPair((Tuple2<Double, Long> t) -> t.swap());
+
+        for (int i = 0; i < percentiles.length; i++) {
+            Double percentile = percentiles[i];
+            if (percentile != null) {
+                long id = (long) (rddSize * percentile);
+                values[i] = indexed.lookup(id).get(0);
+            } else {
+                values[i] = null;
+            }
+        }
+        return values;
+    }
+    
+    private JavaRDD<Double> rowToDouble(Dataset<Row> dataset) {
+        JavaRDD<Row> data = dataset.toJavaRDD();
+        JavaRDD<Double> dataDouble = data.map(new Function<Row,Double>() {
+            public Double call(Row s) {
+                Object value = s.get(0);
+                Double val = null; 
+                if (value != null) {
+                    if (value.getClass().equals(Double.class)) {
+                        val = s.getDouble(0);
+                    } else if (value.getClass().equals(Integer.class)) {
+                        val = (double) s.getInt(0);
+                    } else if (value.getClass().equals(Float.class)) {
+                        val = (double) s.getFloat(0);
+                    } else if (value.getClass().equals(Long.class)) {
+                        val = (double) s.getLong(0);
+                    } else if (value.getClass().equals(Short.class)) {
+                        val = (double) s.getShort(0);
+                    }
+                }
+                return val;
+            }
+        });
+        return dataDouble;
+    }
     
     @Override
     public FloorCeilingMissingModel fit(Dataset<?> dataset) {
-        String[] columns = this.getInputCols();
-        Double[] floors = new Double[columns.length];
+        String[] columns  = this.getInputCols();
+        Double[] floors   = new Double[columns.length];
         Double[] ceilings = new Double[columns.length];
         Double[] missings = new Double[columns.length];
         
         for (int i = 0; i < columns.length; i++) {
             String column = columns[i];
-            List<Row> values = dataset.select(column).collectAsList();
-            double[] valuesDouble = new double[values.size()];
-            double currMin = Double.MAX_VALUE;
-            double currMax = Double.MIN_VALUE;
-            for (int j = 0; j < valuesDouble.length; j++) {
-                Object value = values.get(j).get(0);
-                if (value != null) {
-                    if (value.getClass().equals(Long.class)) {
-                        valuesDouble[j] = values.get(j).getLong(0);
-                    } else if (value.getClass().equals(Double.class)) {
-                        valuesDouble[j] = values.get(j).getDouble(0);
-                    } else if (value.getClass().equals(Integer.class)) {
-                        valuesDouble[j] = values.get(j).getInt(0);
-                    } else if (value.getClass().equals(Float.class)) {
-                        valuesDouble[j] = values.get(j).getFloat(0);
-                    } else if (value.getClass().equals(Short.class)) {
-                        valuesDouble[j] = values.get(j).getShort(0);
-                    }
-                    
-                    // Min
-                    if (valuesDouble[j] < currMin) {
-                        currMin = valuesDouble[j];
-                    }
-                    
-                    // Max
-                    if (valuesDouble[j] > currMax) {
-                        currMax = valuesDouble[j];
-                    }
-                }
-            }
+            Dataset subset = dataset.select(column).where(col(column).isNotNull());
             
-            Percentile perc = new Percentile();
-            perc.setData(valuesDouble);
+            JavaRDD<Double> doubleData = rowToDouble(subset);
+            doubleData.persist(StorageLevel.DISK_ONLY());
             
-            // Lower Percentile
+            Double[] percentiles = new Double[3];
             if (this.hasLowerPercentile()) {
-                double lowerPerc = this.getLowerPercentile()*100;
-                lowerPerc = lowerPerc <= 0  ? 0.0 : lowerPerc;
-                if (lowerPerc == 0.0) {
-                    floors[i] = currMin;
-                } else {
-                    floors[i] = perc.evaluate(lowerPerc);
-                }
+                percentiles[0] = this.getLowerPercentile();
+            } else {
+                percentiles[0] = null;
             }
             
-            // Upper Percentile
             if (this.hasUpperPercentile()) {
-                double upperPerc = this.getUpperPercentile()*100;
-                upperPerc = upperPerc > 100 ? 100 : upperPerc;
-                if (upperPerc == 100.0) {
-                    ceilings[i] = currMax;
-                } else {
-                    ceilings[i] = perc.evaluate(upperPerc);
-                }
+                percentiles[1] = this.getUpperPercentile();
+            } else {
+                percentiles[1] = null;
             }
             
-            // Missing Percentile
             if (this.hasMissingPercentile()) {
-                double missingPerc = this.getMissingPercentile()*100;
-                missingPerc = missingPerc <= 0  ? 0.0 : missingPerc;
-                missingPerc = missingPerc > 100 ? 100 : missingPerc;
-                if (missingPerc == 100.0) {
-                    missings[i] = currMax;
-                } else if (missingPerc == 0.0) {
-                    missings[i] = currMin;
-                } else {
-                    missings[i] = perc.evaluate(missingPerc);
-                }
+                percentiles[2] = this.getMissingPercentile();
+            } else {
+                percentiles[2] = null;
             }
             
-            // Sometimes floor/ceiling can be the same value (for booleans) this forces it so it won't be
-            // TODO: Should make this optional
-            if (!this.get(this.lowerPercentile()).isEmpty() && !this.get(this.upperPercentile()).isEmpty()) {
-                if (floors[i] == ceilings[i]) {
-                    floors[i] = currMin;
-                    ceilings[i] = currMax;
-                }
+            Double[] values = getPercentiles(doubleData, percentiles);
+            
+            if (this.hasLowerPercentile()) {
+                floors[i] = values[0];
+            } else {
+                floors[i] = null;
+            }
+            
+            if (this.hasUpperPercentile()) {
+                ceilings[i] = values[1];
+            } else {
+                ceilings[i] = null;
+            }
+            
+            if (this.hasMissingPercentile()) {
+                missings[i] = values[2];
+            } else {
+                missings[i] = null;
             }
         }
         
         // Generate the model now that we have everything
         FloorCeilingMissingModel fcm = new FloorCeilingMissingModel()
                 .setInputCols(columns);
-        if (!this.get(this.upperPercentile()).isEmpty())  {
-            fcm = fcm.setCeilings(ceilings);
-        }
-        
-        if (!this.get(this.lowerPercentile()).isEmpty()) {
+        if (this.hasLowerPercentile()) {
             fcm = fcm.setFloors(floors);
         }
         
+        if (this.hasUpperPercentile()) {
+            fcm = fcm.setCeilings(ceilings);
+        }
+        
+        if (this.hasMissingPercentile()) {
+            fcm = fcm.setMissings(missings);
+        }
         return fcm;
     }
 
